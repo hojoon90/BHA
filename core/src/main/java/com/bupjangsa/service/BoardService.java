07a2aac46aba18e9753ba9419ff89ff0a378b419
최호준 <hojoon90@gmail.com>
2022-12-18 22:24:43 +0900

    Transaction 서칭. put 기능 완료

--- core/src/main/java/com/bupjangsa/service/BoardService.java
+++ core/src/main/java/com/bupjangsa/service/BoardService.java
@@ -9,12 +9,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class BoardService {
 
     private final SeekPersister seekPersister;
     private final CollectPersister collectPersister;
 
+    @Transactional  //Transaction은 비즈니스가 시작하는 위치에 놓는게 제일 좋다. 자세한 내용은 정리 후 올릴 예정.
     public void postArticle(AllBoard allBoard){
         collectPersister.postArticle(allBoard);
     }
@@ -23,6 +24,7 @@ public class BoardService {
         return seekPersister.selectBoard(boardType, boardNo);
     }
 
+    @Transactional
     public void putArticle(AllBoard allBoard){
         collectPersister.putArticle(allBoard);
     }
--- jpa/src/main/java/com/bupjangsa/repository/CollectPersisterImpl.java
+++ jpa/src/main/java/com/bupjangsa/repository/CollectPersisterImpl.java
@@ -1,10 +1,14 @@
 package com.bupjangsa.repository;
 
 import com.bupjangsa.domain.AllBoard;
+import com.bupjangsa.domain.QAllBoard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class CollectPersisterImpl implements CollectPersister{
 
@@ -14,6 +18,8 @@ public class CollectPersisterImpl implements CollectPersister{
     @PersistenceContext
     EntityManager entityManager;
 
+    LocalDateTime localDateTime = LocalDateTime.now();
+
     public CollectPersisterImpl(JPAQueryFactory jpaQueryFactory, EntityManager entityManager){
         this.jpaQueryFactory = jpaQueryFactory;
         this.entityManager = entityManager;
@@ -22,12 +28,27 @@ public class CollectPersisterImpl implements CollectPersister{
 
     @Override
     public void postArticle(AllBoard allBoard) {
+        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+
+        allBoard.setRegDate(date);
+
         //queryDsl은 insert시엔 EntityManager를 사용한다.
         entityManager.persist(allBoard);
+        entityManager.flush();
     }
 
     @Override
     public void putArticle(AllBoard allBoard) {
+        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
 
+        QAllBoard qallBoard = new QAllBoard("allBoard");
+
+        jpaQueryFactory.update(qallBoard)
+                .where(qallBoard.no.eq(allBoard.getNo()))
+                .where(qallBoard.boardType.eq(allBoard.getBoardType()))
+                .set(qallBoard.title, allBoard.getTitle())
+                .set(qallBoard.content, allBoard.getContent())
+                .set(qallBoard.altDate, date)
+                .execute();
     }
 }
