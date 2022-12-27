017981d8434d0c4da3e0ef7c56786474edc69fa2
최호준 <hojoon90@gmail.com>
2022-12-27 23:29:46 +0900

    회원 정보 작업중

--- controller/src/main/java/com/bupjangsa/controller/UserController.java
+++ controller/src/main/java/com/bupjangsa/controller/UserController.java
@@ -8,33 +8,54 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/user/*")
 public class UserController {
 
+    public ResultMap resultMap;
+
     //TODO SNS 가입 고민 필요.
 
     //사용자 가입
     @PostMapping(value = "/userInfo")
     public ResultMap registUser(){
+        resultMap = new ResultMap();
 
-        return null;
+
+        resultMap.setResultCode(200);
+        resultMap.setResultReason("OK");
+
+        return resultMap;
     }
 
     //회원정보 수정
     @PutMapping(value = "/userInfo")
     public ResultMap updateUser(){
+        resultMap = new ResultMap();
 
-        return null;
+
+        resultMap.setResultCode(200);
+        resultMap.setResultReason("OK");
+
+        return resultMap;
     }
 
     //회원 탈퇴
     @DeleteMapping(value = "/userInfo")
     public ResultMap deleteUser(){
+        resultMap = new ResultMap();
 
-        return null;
+        resultMap.setResultCode(200);
+        resultMap.setResultReason("OK");
+
+        return resultMap;
     }
 
     //회원 조회
     //TODO 필요한 회원 정보 정의 필요.
     @GetMapping(value = "/userInfo")
     public UserInfo getUser(){
+        resultMap = new ResultMap();
+
+
+        resultMap.setResultCode(200);
+        resultMap.setResultReason("OK");
         return null;
     }
 
